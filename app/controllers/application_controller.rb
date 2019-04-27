6ce7e73ff1771c4778cf9f17bd114d5efc48e3a4
Andrew Waller <andrewhwaller@gmail.com>
2019-04-27 04:30:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
 
   end
 
@@ -24,4 +26,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
